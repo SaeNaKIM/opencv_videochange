f71f34eca22fed1b451ae84d81b77b66cb510942
SaeNaKIKM <tosk3927@gmail.com>
2018-06-25 17:08:31 +0900

    header file add

---
+++ VideoChange.h
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+using namespace std;
+using namespace cv;
+
+
+class VideoChange
+{
+
+private : 
+
+	
+	string filename;
+	vector<Mat> sampledImgV;
+	Mat background;
+	Mat changeGraph;
+	ofstream fs;
+
+	 double dstFrameLoc;
+
+	//file information
+	float fps;
+	float cols;
+	float rows;
+	long double nframe;
+
+	//video control
+	bool stopFlag = false;
+	int samplingRate = 30;
+	double sample = 0;
+
+
+
+public :
+
+	//constructor 
+	VideoChange(string filename,VideoCapture cap);
+
+	void videoInfoPrint();
+
+	void samplingVideoFrame(Mat &Frame, double curFrameLoc);  
+
+	void backgroundEstimation( Mat &background, int type );
+	
+	void detectChangeFrame(int detectType, string outfilename); // compute difference between background image and samplec img and Save the interesting image(.png)
+
+	void writeTime(Mat &frame, string timeContent); // write time to image 
+
+	void writeTime(ofstream fs, string timeContent); 
+
+	void drawChangeGraph(int sampleNum, double changeRate); // parameter를 어떻게 만들 것인가 생각 해야함...
+
+	void setFilename(string filename);
+
+	void setFimeStream();
+
+	void preprocess(Mat &src, Mat &dst);
+
+	void videoControl(int key, double curFrameLoc);
+
+	bool isStop();
+
+	double getFrameLoc();
+
+	void setSamplingRate(int samplingRate);
+
+	void clearsampledImgV();
+
+	void setChangeGraph(int width);
+};
