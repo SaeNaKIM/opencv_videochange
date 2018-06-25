6ee467fc69fab92d601d301cd669479faa98e6d2
SaeNaKIKM <tosk3927@gmail.com>
2018-06-25 16:09:57 +0900

    first commit

---
+++ VideoChange.cpp
@@ -0,0 +1,215 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include <opencv2/opencv.hpp>
+#include <iostream> 
+#include <vector>
+#include "VideoChange.h"
+
+
+VideoChange::VideoChange(string filename, VideoCapture cap) {
+
+	setFilename(filename);
+	this->fps = cap.get(CV_CAP_PROP_FPS);
+	this->cols = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+	this->rows = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+	this->nframe = cap.get(CV_CAP_PROP_FRAME_COUNT);
+
+}
+
+void VideoChange::videoInfoPrint() {
+
+	
+	/*-------------Output video information-------------------*/
+	cout << endl;
+	cout << "Input Video File Name : " << this->filename << endl;
+	cout << "Frame Per Seconds : " << this->fps << endl;
+	cout << "Frame Size : " << this->cols << " x " << rows << endl;
+	cout << "Frame Count : " << this->nframe << endl;
+	/*--------------------------------------------------------*/
+
+}
+void VideoChange:: preprocess(Mat &src, Mat &dst) {
+
+	cvtColor(src, src, CV_BGR2GRAY); // convert channel 3 to 1 
+	resize(src, src, Size(src.cols / 2, src.rows / 2));
+	GaussianBlur(src, dst, Size(3, 3), 0); //Blurring 
+
+}
+// sampling rate, videofile path,  output - sampledImg
+void VideoChange::samplingVideoFrame(Mat &frame ,double curFrameLoc) {
+
+	if ((int)curFrameLoc % (int)this->sample == 0) {
+			
+		sampledImgV.push_back(frame); // 언제 clear 해줄건지 정해야함. * 벡터를 신중히 다룹시다..
+	
+	}
+
+} 
+void VideoChange::backgroundEstimation(Mat &background, int type ) {
+	
+	Mat sum(sampledImgV[0].size(), CV_64FC1);
+	double sampledImgL = sampledImgV.size();
+
+
+
+	if (type == 1) { // mean 
+
+		for (int i = 0; sampledImgV.size(); i++) {
+
+			accumulateWeighted(sampledImgV[i], sum, 1.0/sampledImgL);
+		
+		}
+		this->background = sum;
+
+	}
+	else if (type == 2) { // median
+	
+	
+	
+	}
+	else if (type == 3) { // gaussian of mixture 
+
+
+
+	}
+	else {
+
+		cout << "wrong type" << endl;
+		return;
+	}
+
+	
+	background = this->background;
+	return;
+}
+// compute difference between background image and sampled image and Save the interesting image(.png) and data(.txt)
+void VideoChange::detectChangeFrame(int detectType, string outfilename){
+
+
+	int nonZeroCnt = 0;
+	double changeRate = 0.0f;
+	double pixelThreshold = 0.2;
+	int imgSize = sampledImgV[0].rows * sampledImgV[0].cols;
+	
+	Mat absImg; 
+	ofstream outFile("output_video_change_rate.txt"); // default
+	//ofstream outFile(outputfilename);
+	
+
+	if (detectType == 1) { // pixel 
+		
+		setChangeGraph(sampledImgV.size()); // initialize chage graph
+
+		for (int i = 0; sampledImgV.size(); i++) {
+
+			absImg = abs(sampledImgV[i] - this->background);
+			medianBlur(absImg, absImg,3); //For delete noise 
+			nonZeroCnt = countNonZero(absImg);
+			changeRate = (double)nonZeroCnt / imgSize;
+
+			if (changeRate > pixelThreshold) {
+				
+				// store image and time of the frame 
+				string filename = "frame_time" + to_string(i / samplingRate ) + ".png";
+				imwrite(filename,sampledImgV[i]);
+				imshow("absImg", absImg);
+				waitKey(0);
+
+			}
+
+			drawChangeGraph(i,changeRate);
+			outFile << i * sample << " " << changeRate << "\n"; // n-th frame 
+		}
+
+	}
+	else if (detectType == 2) { //blob - threshold 필요
+		
+		
+	}
+
+	outFile.close();
+	return;
+}
+void VideoChange::writeTime(Mat &frame, string timeContent) {
+
+
+}
+
+void VideoChange::writeTime(ofstream fs, string timeContent){
+
+
+}
+
+// parameter를 어떻게 만들 것인가 생각 해야함 
+void VideoChange::drawChangeGraph(int sampleNum, double changeRate) {
+
+	resize(this->changeGraph, this->changeGraph, Size(sampledImgV.size(),100));
+	circle(this->changeGraph, Point2f(sampleNum, changeRate * 100), 1, Scalar(255, 0, 0));
+}
+
+void VideoChange::setFilename(string filename) {
+
+	this->filename = filename;
+}
+
+void VideoChange::setFimeStream() {
+
+
+
+}
+void VideoChange::videoControl(int key, double curFrameLoc)
+{
+	this->dstFrameLoc = curFrameLoc;
+
+	
+	if (key == 32)
+	{
+		//stop Toggle
+		stopFlag = (stopFlag == true ? false : true);
+		
+	}
+	else if (key == ']')
+	{
+		this->dstFrameLoc += 100;
+	}
+	else if (key == '[')
+	{
+		this->dstFrameLoc -= 100;
+	}
+	else if (key == '.')
+	{
+		this->dstFrameLoc += 10;
+	}
+	else if (key == ',')
+	{
+		this->dstFrameLoc -= 10;
+	}
+
+	return ;
+}
+
+bool VideoChange::isStop()
+{
+	return stopFlag;
+}
+
+double VideoChange::getFrameLoc()
+{
+	return dstFrameLoc - 1;
+}
+
+void VideoChange::setSamplingRate(int samplingRate)
+{
+	this->samplingRate = samplingRate;
+	this->sample = this->fps / this->samplingRate;
+}
+
+void VideoChange::clearsampledImgV()
+{
+	this->sampledImgV.clear();
+}
+
+void VideoChange::setChangeGraph(int width)
+{
+	this->changeGraph = Mat::zeros(100, width, CV_8UC3);
+}
---
+++ VideoChange_main.cpp
@@ -0,0 +1,88 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "VideoChange.h"
+
+
+using namespace std;
+using namespace cv;
+
+
+// backgrund subtraction algorithm option
+const int BG_MEAN = 1;
+const int BG_MEDIAN = 2;
+const int BG_MOG = 3;
+
+
+//change notification parameter option
+#define DT_PIXEL =  1;
+#define DT_BLOB = 2; 
+
+
+const int SampleRate = 1;
+double curFrameLoc = 0.0;
+
+int main() {
+
+	string videoFilename = "C:\\Users\\Dev3Team\\Documents\\2.h264";
+	VideoCapture cap(videoFilename);	
+	VideoChange VC(videoFilename,cap);
+	Mat frame;
+	Mat background;
+
+
+	int key;
+
+	
+	if (!cap.isOpened()) {
+		cerr << "Video File Open Error" << endl;
+		exit(1);
+	}
+
+
+	VC.videoInfoPrint();
+	VC.setSamplingRate(SampleRate);
+
+	while (1)
+	{
+		if (!VC.isStop())
+		{
+			if (!cap.read(frame))
+				break;
+
+			//curFrameLoc = cap.get(CAP_PROP_POS_FRAMES);
+			//cout << (double)cap.get(CAP_PROP_POS_MSEC) << endl;
+			curFrameLoc++;
+			VC.preprocess(frame, frame);
+			
+			VC.samplingVideoFrame(frame, curFrameLoc);
+			imshow("current Frame", frame);
+		}
+
+		
+		key = waitKey(60);
+		if (key == 27) {
+			cout << "terminated during execution" << endl;
+			break;
+		}
+		else {
+			VC.videoControl(key, curFrameLoc);
+			cap.set(CV_CAP_PROP_POS_FRAMES, VC.getFrameLoc());
+		}
+
+	}
+
+	VC.backgroundEstimation(background,BG_MEAN);
+	imshow("background", background);
+
+
+
+	
+	waitKey(0);
+	destroyAllWindows();
+	VC.clearsampledImgV();
+	cap.release();
+	
+	return 0;
+}
+
