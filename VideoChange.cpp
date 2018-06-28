a6278525e3ed46f0222f0392dc4f84e43d4c5be0
SaeNaKIKM <tosk3927@gmail.com>
2018-06-28 16:44:22 +0900

    0628 presentation version, mp4

--- VideoChange.cpp
+++ VideoChange.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 #include <direct.h>
+#include <thread>
 #include "VideoChange.h"
 
 
@@ -29,21 +30,19 @@ void VideoChange::videoInfoPrint() {
 }
 void VideoChange::preprocess(Mat &src, Mat &dst) {
 
-	cvtColor(src, src, CV_BGR2GRAY); // convert channel 3 to 1 
-	resize(src, src, Size(src.cols / 2, src.rows / 2));
-	GaussianBlur(src, dst, Size(3, 3), 0); //Blurring 
+	Mat gray;
+	Mat resizeMat;
+
+	cvtColor(src, gray, CV_BGR2GRAY); // convert channel 3 to 1 
+	resize(gray, resizeMat, Size(gray.cols / 4, gray.rows / 4));
+	GaussianBlur(resizeMat, dst, Size(3, 3), 0); //Blurring 
 
 }
-// sampling rate, videofile path,  output - sampledImg
-void VideoChange::samplingVideoFrame(Mat frame, double curFrameLoc) {
+void VideoChange::samplingVideoFrame(Mat frame) {
 
 	Mat tmp;
 	tmp = frame.clone();
-	if (((int)curFrameLoc % (int)this->sample) == 0) {
-
-		sampledImgV.push_back(tmp); // 언제 clear 해줄건지 정해야함. * 벡터를 신중히 다룹시다..
-
-	}
+	sampledImgV.push_back(tmp); // 언제 clear 해줄건지 정해야함. * 벡터를 신중히 다룹시다..
 
 }
 void VideoChange::backgroundEstimation(Mat &background, int type) {
@@ -61,24 +60,9 @@ void VideoChange::backgroundEstimation(Mat &background, int type) {
 
 
 			accumulateWeighted(sampledImgV[i], sum, 1.0 / sampledImgL);
-			//sampledImgV[i].convertTo(temp, CV_32FC1);
-			//temp = temp / sampledImgL;
-			//sum += temp;
-			//accumulate(sampledImgV[i], sum);
-			//imshow("sum", sum);
-			//waitKey(30);
-
 		}
 
 		convertScaleAbs(sum, this->background);
-		// cout << this->background;
-		//this->background = sum / sampledImgL;
-		//sum.convertTo(avg, CV_8UC1, 1.0 / sampledImgL);
-		//this->background = sum / sampledImgL;
-		//imshow("background in function", avg);
-		//waitKey(0);
-		//cout << "background channgel" << this->background.channels() << "type: " << this->background.type() << "\n";
-
 
 	}
 	else if (type == 2) { // median of median 
@@ -98,34 +82,25 @@ void VideoChange::backgroundEstimation(Mat &background, int type) {
 		return;
 	}
 
-	//imshow("background in function", this->background); //debug
+	//imshow("background in function", this->background); waitKey(0); //debug
 	background = this->background.clone();
-	//background = avg.clone();
 	return;
 }
-
 // compute difference between background image and sampled image. Save the interesting image(.png) and data(.csv)
 void VideoChange::detectChangeFrame(int detectType, string outfilename, double pixelThreshold) {
 
 
 	int nonZeroCnt = 0;
 	double changeRate = 0.0f;
-	//double pixelThreshold = 0.06;
 	int imgSize = sampledImgV[0].rows * sampledImgV[0].cols;
 
 	Mat absImg;
-	Mat absImg2;
-	Mat abs_or;
-	Mat norm_frame = Mat::zeros(sampledImgV[0].size(), CV_32FC1);;
-	Mat norm_bg = Mat::zeros(this->background.size(),CV_32FC1);
-	Mat norm_abs;
-	Mat norm_abs2;
-	Mat log_abs;
 	Mat thresholdImg;
 	Mat hist_frame;
 
-	ofstream outFile(outfilename);
-	char directory[] = { "C:\\Users\\Dev3Team\\source\\repos\\opencv_test1\\opencv_test1\\video_change" };
+	ofstream outFile(outfilename + ".csv");
+	char directory[] = {"video_change"};
+	
 	int nResult = _mkdir(directory);
 
 	if (nResult == 0)
@@ -139,67 +114,32 @@ void VideoChange::detectChangeFrame(int detectType, string outfilename, double p
 
 	if (detectType == 1) { // pixel 
 
-		setChangeGraph(sampledImgV.size()); // initialize chage graph
-		//this->background.convertTo(this->background, CV_32FC1);
-		//normalize(this->background, norm_bg, 1.0, 0.0, NORM_MINMAX);
 		equalizeHist(this->background, this->background);
-		imshow("equalized background", this->background);
 
-		// cout << norm_bg;
 		for (int i = 0; i < sampledImgV.size(); i++) {
 
 			//cout << "channel: " << sampledImgV[i].channels() << this->background.channels() << endl; // debug
-			
-			//sampledImgV[i].convertTo(sampledImgV[i], CV_32FC1);
-			//normalize(sampledImgV[i], norm_frame, 1.0, 0.0, NORM_MINMAX); // normalize data
-			
 			equalizeHist(sampledImgV[i], hist_frame);
 			absImg = abs(hist_frame - this->background);
 			
-			//absImg = abs(norm_frame - norm_bg);
-			//absImg = norm_frame - norm_bg;
-			//absImg2 = norm_bg- norm_frame;
-
-			//bitwise_or(absImg, absImg2, abs_or);
-
-			//medianBlur(absImg, absImg, 3);
-			//medianBlur(absImg2, absImg2, 3);
-			//medianBlur(abs_or, abs_or, 3);
-
 			threshold(absImg, thresholdImg, 50, 255, THRESH_BINARY);
 			nonZeroCnt = countNonZero(thresholdImg);
 			changeRate = (double)nonZeroCnt / imgSize;
 			//cout << "change rate: " << changeRate << "\n";
-
 		
 			if (changeRate > pixelThreshold) {
 
 				// store image and time of the frame 
-				string filename = string(directory) + "\\frame_" + to_string(i*this->sample/60) + ".png";
+				string filename = string(directory) + "\\frame_" + to_string(i*this->sample/ this->fps) + ".png";
 				imwrite(filename, sampledImgV[i]);
 
 				//debugging    
 				//imshow("change image", sampledImgV[i]);
-				//absImg.convertTo(absImg, CV_8UC1);
-				
-				//normalize(absImg, norm_abs, 255, 0, NORM_MINMAX);
-				//normalize(absImg2,norm_abs2, 255, 0, NORM_MINMAX);
-				//log(log_abs, log_abs);
-
-				//imshow("norm abs image", absImg);
-				//normalize(thresholdImg, thresholdImg, 255, 0, NORM_MINMAX);
+				//imshow("abs image", absImg);
 				//imshow("threshold image", thresholdImg);
-				//cout << absImg;
-				//cout << "\n";
-				//imshow("norm abs image2", norm_abs2);
-				//imshow("log abs image", log_abs);
-
-
 				//waitKey(0);
 			}
-
-			//drawChangeGraph(i, changeRate);
-			outFile << to_string(i*this->sample / 60) << "," << changeRate << "\n"; // n-th frame 
+			outFile << to_string(i*this->sample / this->fps) << "," << changeRate << "\n"; // n-th frame 
 		}
 
 	}
@@ -208,7 +148,6 @@ void VideoChange::detectChangeFrame(int detectType, string outfilename, double p
 
 	}
 
-	//imshow("change graph", changeGraph);
 	outFile.close();
 	return;
 }
@@ -218,23 +157,15 @@ string VideoChange::computeTime(int curFrameLoc)
 	int min = (int)curFrameLoc * sample / 60;
 	int sec = ((double)curFrameLoc * sample / 60.0 - min) * 60.0;
 	string current_time = to_string(min) + "." + to_string(sec);
-	
+
 	return current_time;
 }
-
-
-// parameter를 어떻게 만들 것인가 생각 해야함 
-void VideoChange::drawChangeGraph(int sampleNum, double changeRate) {
-
-	circle(this->changeGraph, Point2f(sampleNum, changeRate * 100), 1, Scalar(255, 0, 0));
-}
-
 void VideoChange::setFilename(string filename) {
 
 	this->filename = filename;
 }
 
-void VideoChange::videoControl(int key, double curFrameLoc)
+void VideoChange::controlVideo(int key, double curFrameLoc)
 {
 	this->dstFrameLoc = curFrameLoc;
 
@@ -275,7 +206,7 @@ double VideoChange::getFrameLoc()
 	return dstFrameLoc - 1;
 }
 
-void VideoChange::setSamplingRate(int samplingRate)
+void VideoChange::setSamplingRate(double samplingRate)
 {
 	this->samplingRate = samplingRate;
 	this->sample = this->fps / this->samplingRate;
@@ -285,8 +216,7 @@ void VideoChange::clearsampledImgV()
 {
 	this->sampledImgV.clear();
 }
-
-void VideoChange::setChangeGraph(int width)
+double VideoChange::getSampleNumber()
 {
-	this->changeGraph = Mat::zeros(100, width, CV_8UC3);
+	return this->sample;
 }
--- VideoChange_main.cpp
+++ VideoChange_main.cpp
@@ -17,78 +17,76 @@ const int BG_MOG = 3;
 const int DT_PIXEL =  1;
 const int DT_BLOB = 2;
 
-
-
 int main() {
 
 	int key = 0 ;
-	int SampleRate = 1;
-	double curFrameLoc = 0.0;
-	
+	double SampleRate = 1;
+	int curFrameLoc = 0.0;
+	int sample = 0;
+	double sum = 0;
+
 	Mat frame;
 	Mat grayFrame;
 	Mat background;
 
-	clock_t begin, end1, end2;
+	clock_t begin, read_begin, read_end, sampling_end, total_end;
 
-	string videoFilename = "C:\\Users\\Dev3Team\\Documents\\2.h264";
-	//string videoFilename = "C:\\Users\\Dev3Team\\Desktop\\Video_1.mp4";
+	string videoFilename = "C:\\Users\\Dev3Team\\Documents\\2.mp4";
 	VideoCapture cap(videoFilename);
-	VideoChange VC(videoFilename, cap);
-
+	
 	if (!cap.isOpened()) {
 		cerr << "Video File Open Error" << endl;
 		exit(1);
 	}
 
-
+	VideoChange VC(videoFilename, cap);
 	VC.videoInfoPrint(); 
 	VC.setSamplingRate(SampleRate);
-
+	sample = VC.getSampleNumber();
 	begin = clock();
 
-	//video play
+	//video sampling
 	while (1)
 	{
-		if (!VC.isStop())
-		{
-			if (!cap.read(frame))
-				break;
+		read_begin = clock();
 
-			//curFrameLoc = cap.get(CAP_PROP_POS_FRAMES); //왜 안되냐 빡치게...
-			//cout << cap.get(CAP_PROP_POS_MSEC) << endl;
-			curFrameLoc++;
-			VC.preprocess(frame, grayFrame);	
-			VC.samplingVideoFrame(grayFrame, curFrameLoc);		
-			//imshow("current Frame", grayFrame);
-		}
-
-		
-		//key = cv::waitKey(10);
-		if (key == 27) {
-			cout << "terminated during video execution" << endl;
+		cap.set(CV_CAP_PROP_POS_FRAMES, curFrameLoc * sample);
+		curFrameLoc++;
+		if (!cap.read(frame))
 			break;
-		}
-		else {
-			VC.videoControl(key, curFrameLoc);
-			cap.set(CV_CAP_PROP_POS_FRAMES, VC.getFrameLoc());
+
+		read_end = clock();
+		sum += read_end - read_begin;
+		
+		VC.preprocess(frame, grayFrame);
+		VC.samplingVideoFrame(grayFrame);
+			
+		imshow("sampling Frame", grayFrame);
+
+		key = waitKey(1);
+		if (key == 27) { //ESC
+			cout << "terminated during video sampling" << endl;
+			break;
 		}
 
 	}
 
-	end1 = clock();
-
+	sampling_end = clock();
 	VC.backgroundEstimation(background, BG_MEAN);
+	
+	//debugging background result
 	//imshow("background", background);
 	//waitKey(0);
 
- 	VC.detectChangeFrame(DT_PIXEL, "video_change.csv", 0.06 );
+ 	VC.detectChangeFrame(DT_PIXEL, "video_change", 0.06 );
+	total_end = clock();
 
-	end2 = clock();
-
-	cout << "total frame: " << curFrameLoc << endl;
-	cout << "sampling execution time:" << (end1 - begin) / CLOCKS_PER_SEC << endl;
-	cout << "total execution time:" << (end2 - begin) / CLOCKS_PER_SEC << endl;
+	// image processing result 
+	cout << "sampling frame: " << curFrameLoc << endl;
+	cout << "image read execution time:" << sum / CLOCKS_PER_SEC  << endl;
+	cout << "average read execution time per frame:" << sum / CLOCKS_PER_SEC / curFrameLoc << endl;
+	cout << "sampling execution time:" << (sampling_end - begin) / CLOCKS_PER_SEC << endl;
+	cout << "total execution time:" << (total_end - begin) / CLOCKS_PER_SEC << endl;
 	system("pause");
 
 	//release resource
